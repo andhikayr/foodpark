4505e1cdd828a7def9ac724a9814138320c24d69
andhikayr <andhikayr9@gmail.com>
2024-09-28 20:08:10 +0700

    tambah file terjemahan en dan id

---
+++ lang/id/form.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'judul' => 'Pendaftaran Mahasiswa',
+    'input' => [
+        'nim' => 'NIM',
+        'nama_lengkap' => 'Nama Lengkap',
+        'email' => 'Email',
+        'jenis_kelamin' => 'Jenis Kelamin',
+        'pilihan_jenis_kelamin' => [
+            'laki_laki' => 'Laki-Laki',
+            'perempuan' => 'Perempuan',
+        ],
+        'jurusan' => 'Jurusan',
+        'alamat' => 'Alamat',
+        'tombol' => 'Daftar',
+    ],
+    'nama_kampus' => 'Universitas Ilkoom',
+];
