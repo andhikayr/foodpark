9e865184d22a77d1ca13c39ff2c6dca51d7ac869
andhikayr <andhikayr9@gmail.com>
2024-09-29 09:13:39 +0700

    membuat tombol logout berfungsi pada halaman admin

--- resources/views/admin/layouts/navbar.blade.php
+++ resources/views/admin/layouts/navbar.blade.php
@@ -26,10 +26,11 @@
                     <div class="dropdown-divider"></div>
 
                     <!-- item-->
-                    <a href="javascript:void(0);" class="dropdown-item notify-item">
-                        <i class="fe-log-out"></i>
-                        <span>Logout</span>
-                    </a>
+                    <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" class="dropdown-item notify-item"><i class="fe-log-out"></i>Logout</a>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="post" class="d-none">
+                        @csrf
+                    </form>
 
                 </div>
             </li>
