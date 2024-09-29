8c9c2ac94f0fbb697ac8382f6fe2ed41a536be06
andhikayr <andhikayr9@gmail.com>
2024-09-29 11:46:25 +0700

    membuat form login admin panel

---
+++ app/Http/Controllers/Admin/AuthController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login()
+    {
+        return view('admin.auth.login');
+    }
+
+    public function forget_password()
+    {
+        return view('admin.auth.forget-password');
+    }
+}
