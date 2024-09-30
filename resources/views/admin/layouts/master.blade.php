7f65b4e6dbcd1674724146e89d6ae4253026c07f
andhikayr <andhikayr9@gmail.com>
2024-09-30 14:54:42 +0700

    menghapus sidebar template yg belum diperlukan dan menambah "dashboard" pada judul halaman admin

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -4,7 +4,7 @@
     <head>
 
         <meta charset="utf-8" />
-        <title>@yield('title') | FoodPark</title>
+        <title>@yield('title') | FoodPark Dashboard</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
         <meta content="Andhika Yr" name="author" />
