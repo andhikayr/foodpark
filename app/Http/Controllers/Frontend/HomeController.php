43032700569ff04d664f455c080f0c255329bc7d
andhikayr <andhikayr9@gmail.com>
2024-10-03 18:09:38 +0700

    tampilkan judul dan card "mengapa memilih kita" pada frontend

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -3,15 +3,25 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\SectionTitle;
 use App\Models\Slider;
+use App\Models\WhyChooseUs;
 use Illuminate\Contracts\View\View;
-use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
 
 class HomeController extends Controller
 {
     public function index() : View
     {
         $sliders = Slider::where('status', 1)->get();
-        return view('frontend.index', compact('sliders'));
+        $sectionTitles = $this->getSectionTitles();
+        $whyChooseUs = WhyChooseUs::where('status', 1)->get();
+        return view('frontend.index', compact('sliders', 'sectionTitles', 'whyChooseUs'));
+    }
+
+    public function getSectionTitles() : Collection
+    {
+        $keys = ['why_choose_us_title', 'why_choose_us_sub_title', 'why_choose_us_description'];
+        return SectionTitle::whereIn('key', $keys)->pluck('value', 'key');
     }
 }
