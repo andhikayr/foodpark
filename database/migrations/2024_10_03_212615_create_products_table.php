16a85eaea7cc4915760e1fccd2d110c1faf713b2
andhikayr <andhikayr9@gmail.com>
2024-10-03 21:53:50 +0700

    membuat tabel yang berkaitan dengan produk

---
+++ database/migrations/2024_10_03_212615_create_products_table.php
@@ -0,0 +1,76 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug');
+            $table->boolean('status')->default(1);
+            $table->boolean('show_at_home')->default(0);
+            $table->timestamps();
+        });
+
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id')->constrained('product_categories')->onDelete('cascade');
+            $table->string('name');
+            $table->string('slug');
+            $table->string('thumb_image');
+            $table->text('short_description');
+            $table->longText('description');
+            $table->integer('price');
+            $table->integer('offer_price')->nullable();
+            $table->string('sku');
+            $table->boolean('status')->default(1);
+            $table->boolean('show_at_home')->default(0);
+            $table->string('seo_title')->nullable();
+            $table->string('seo_description')->nullable();
+            $table->timestamps();
+        });
+
+        Schema::create('product_galleries', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')->constrained('products')->onDelete('cascade');
+            $table->string('image');
+            $table->timestamps();
+        });
+
+        Schema::create('product_sizes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')->constrained('products')->onDelete('cascade');
+            $table->string('name');
+            $table->integer('price');
+            $table->timestamps();
+        });
+
+        Schema::create('product_options', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')->constrained('products')->onDelete('cascade');
+            $table->string('name');
+            $table->integer('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_categories');
+        Schema::dropIfExists('products');
+        Schema::dropIfExists('product_galleries');
+        Schema::dropIfExists('product_sizes');
+        Schema::dropIfExists('product_options');
+    }
+};
